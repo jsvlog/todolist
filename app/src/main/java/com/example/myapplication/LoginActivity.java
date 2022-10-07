cf0549c9551cfa74b446332465b0b2b6b9ccb820
COA PC <lizardojohnsonc@gmail.com>
2022-10-07 13:38:09 +0800

    log in page done

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView loginQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        loginQuestion = findViewById(R.id.loginQuestion);
+
+        loginQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -0,0 +1,86 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+    TextView registerQuestion;
+    EditText registerEmail, registerPassword, registerPasswordConfirm;
+    Button registerButton;
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_register);
+
+        registerQuestion = findViewById(R.id.registerQuestion);
+        registerEmail = findViewById(R.id.registerEmail);
+        registerPassword = findViewById(R.id.registerPassword);
+        registerPasswordConfirm = findViewById(R.id.registerPasswordConfirm);
+        registerButton = findViewById(R.id.registerButton);
+
+
+        registerQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+
+
+    }
+
+    private void registerUser() {
+        String emailText = registerEmail.getText().toString();
+        String passwordText = registerPassword.getText().toString();
+        String passwordText2 = registerPasswordConfirm.getText().toString();
+        mAuth = FirebaseAuth.getInstance();
+
+
+        if(emailText.isEmpty()){
+            registerEmail.setError("please Enter email");
+        }else if (passwordText.isEmpty()){
+            registerPassword.setError("please Enter password");
+        }else if (passwordText.length() < 6){
+            registerPassword.setError("password should atleast 6 character");
+        }else if (!passwordText.equals(passwordText2)){
+           registerPasswordConfirm.setError("password confirmation did not match!");
+        }mAuth.createUserWithEmailAndPassword(emailText,passwordText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(RegisterActivity.this, "Login Succesful", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+    }
+
+
+}
